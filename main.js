d4c4f424eb859950f2a3a240ff68d176c1d8e79c
Briayan <briayang1997@gmail.com>
2023-08-06 22:05:48 -0400

    Version 0.2

--- main.js
+++ main.js
@@ -12,18 +12,18 @@ for( let i = 1; i < 51; i++)
 function crearPokemon(pokemon)
 {
     const div = document.createElement("div");
-    div.classList.add(".card__container");
+    div.classList.add("card__container");
     div.innerHTML = `<div class="card__container--id-health">
-                        <p class="card__container--id">${pokemon.id}</p>
-                        <p class="card__container--id-health">${pokemon.base_stat}</p>
+                        <p class="card__container--id">#${pokemon.id}</p>
+                        <p class="card__container--health">${pokemon.base_experience}XP</p>
                     </div>
                     <div class="card__container--img">
                         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
                     </div>
                     <p class="card__container--name">${pokemon.name}</p>
                     <div class="card__container--weight-height">
-                        <p class="card__container-height">${pokemon.height}</p>
-                        <p class="card__container-weight">${pokemon.weight}</p>
+                        <p class="card__container-height">${pokemon.height}M</p>
+                        <p class="card__container-weight">${pokemon.weight}KG</p>
                     </div>`;
 
     pokemonContainer.append(div);
