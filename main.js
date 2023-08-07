b0d978189247bb94faeeaf2c5d49ca605b7a4cea
Briayan <briayang1997@gmail.com>
2023-08-06 22:20:12 -0400

    Version 1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const pokemonContainer = document.querySelector('#pokemon__container')
 
 const URL= "https://pokeapi.co/api/v2/pokemon/"
 
-for( let i = 1; i < 51; i++)
+for( let i = 1; i < 52; i++)
 {
     fetch(URL+ i)
         .then((response) => response.json())
